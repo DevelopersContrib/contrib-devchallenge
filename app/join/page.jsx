116c4a14c99f29f5336e5bb88e33482771482c86
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-04-17 09:43:52 +0800

    Join Page Update

--- app/join/page.jsx
+++ app/join/page.jsx
@@ -4,8 +4,30 @@ const page = () => {
   const domain = getDomain();
   return (
     <>
-      <Script src={"//js.hsforms.net/forms/embed/v2.js"} />
       <div className="join-script"></div>
+      
+<script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    
+                    var c=document.createElement("script");
+                    c.type="text/javascript",
+                    c.readyState ? c.onreadystatechange=function(){
+                        "loaded"!=c.readyState&&"complete"!=c.readyState||(c.onreadystatechange=null,b())
+                      }:
+                      c.onload=function(){
+                        hbspt.forms.create({
+                          region: "na1",
+                          portalId: "42816889",
+                          formId: "025de5fa-0bbb-48ce-a056-4da9acc75cd1"
+                        });
+
+                      },
+                    c.src="//js.hsforms.net/forms/embed/v2.js",
+                    document.getElementsByTagName("head")[0].appendChild(c)
+                  `,
+            }}
+          ></script>
     </>
   )
 }
