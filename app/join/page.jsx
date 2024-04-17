de933d924c95b8c7042da62762cc76ce0b4c4fc2
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-04-17 12:35:49 +0800

    Join Update

--- app/join/page.jsx
+++ app/join/page.jsx
@@ -4,30 +4,34 @@ const page = () => {
   const domain = getDomain();
   return (
     <>
-      <div className="join-script"></div>
       
-<script
-            dangerouslySetInnerHTML={{
-              __html: `
-                    
-                    var c=document.createElement("script");
-                    c.type="text/javascript",
-                    c.readyState ? c.onreadystatechange=function(){
-                        "loaded"!=c.readyState&&"complete"!=c.readyState||(c.onreadystatechange=null,b())
-                      }:
-                      c.onload=function(){
-                        hbspt.forms.create({
-                          region: "na1",
-                          portalId: "42816889",
-                          formId: "025de5fa-0bbb-48ce-a056-4da9acc75cd1"
-                        });
+        <div className="row">
+          <div className="col">
+            <div className="join-script"></div>
+          </div>
+        </div>
+        <script
+        dangerouslySetInnerHTML={{
+        __html: `
 
-                      },
-                    c.src="//js.hsforms.net/forms/embed/v2.js",
-                    document.getElementsByTagName("head")[0].appendChild(c)
-                  `,
-            }}
-          ></script>
+        var c=document.createElement("script");
+        c.type="text/javascript",
+        c.readyState ? c.onreadystatechange=function(){
+        "loaded"!=c.readyState&&"complete"!=c.readyState||(c.onreadystatechange=null,b())
+        }:
+        c.onload=function(){
+        hbspt.forms.create({
+        region: "na1",
+        portalId: "42816889",
+        formId: "025de5fa-0bbb-48ce-a056-4da9acc75cd1"
+        });
+
+        },
+        c.src="//js.hsforms.net/forms/embed/v2.js",
+        document.getElementsByTagName("head")[0].appendChild(c)
+        `,
+        }}
+        ></script>
     </>
   )
 }
