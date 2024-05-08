58407fc81b710b084b87deaced2125353afba6d0
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-05-08 10:45:42 +0800

    Update page.jsx

--- app/templates/page.jsx
+++ app/templates/page.jsx
@@ -49,7 +49,12 @@ const Page = () => {
                           <p className="text-xl text-gray-900">{template.name}</p>
                           <p className="text-sm text-gray-500">{template.description}</p>
                         </div>
-                      </a>
+                        <div className='relative'>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute bottom-2 right-2 w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                          </svg>
+                        </div>
+                      </a>                     
                     </div>
                   </div>
                 ))}
